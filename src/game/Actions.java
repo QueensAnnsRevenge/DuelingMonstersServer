b9433e40ed80dec35c51d0b10074ab91d20f0bac
AsusG14/Dusan_Markovic <dusanmarkovic031@gmail.com>
2021-12-03 13:38:33 +0100

    Dodao pocetak akcija

---
+++ src/game/Actions.java
@@ -0,0 +1,13 @@
+package game;
+
+import core.Monster;
+
+public class Actions {
+    public void attac(Monster attacer, Monster defender){
+
+        if(attacer.getAttackPoints() < defender.getDefendPoints()){
+
+        }
+
+    }
+}
