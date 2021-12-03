e68be7d901fa56986b3aa7282fe92dba947ee13d
AsusG14/Dusan_Markovic <dusanmarkovic031@gmail.com>
2021-12-03 15:43:07 +0100

    Zavrsio attack metodu i dodao attackPlayer

--- src/game/Actions.java
+++ src/game/Actions.java
@@ -1,13 +1,25 @@
 package game;
 
+import Exceptions.CanNotAttac;
+import Exceptions.MonsterDestroyed;
+import Exceptions.YouAreDead;
 import core.Monster;
 
 public class Actions {
-    public void attac(Monster attacer, Monster defender){
+    public void attac(Monster attacker, Monster defender) throws CanNotAttac, MonsterDestroyed {
 
-        if(attacer.getAttackPoints() < defender.getDefendPoints()){
-
+        if(attacker.getAttackPoints() < defender.getDefendPoints()){
+            throw new CanNotAttac("Cannot attack");
+        } else {
+            defender.damage(attacker.getAttackPoints());
         }
+    }
 
+    public void attackPlayer(Player defender, Monster attacker) throws YouAreDead {
+        if(defender.getLifePoints() - 1 == 0){
+            throw new YouAreDead("Yout died");
+        } else {
+            defender.decLifePoints();
+        }
     }
 }
